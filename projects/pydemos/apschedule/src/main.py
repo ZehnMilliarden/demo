bad04bcf0a41fc84788f5400a9ac419cbf8b2583
ZehnMilliarden <git@lxf.science>
2023-03-31 14:02:22 +0800

    【projects】add cppdemos

--- projects/pydemos/apschedule/src/main.py
+++ projects/pydemos/apschedule/src/main.py
@@ -3,6 +3,9 @@ from time import sleep
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
+from blinker import signal
+from multiprocessing import Pipe
+import os
 
 class TestScheduler(object):
 
@@ -13,15 +16,20 @@ class TestScheduler(object):
             hour='6-18',
             minute='0-59',
             second='0,10,30,40,50')
+        self.__pip_r__,self.__pip_w__ = os.pipe()
 
-    def my_job(self, text) -> None:
+    def __my_job(self, text) -> None:
         print(f'{text} , current time is {datetime.now()}')
 
     def add_job(self) ->None:
-        self.__sched.add_job(func=TestScheduler.my_job, trigger=self.__trigger,args=[self, 'text'])
+        self.__sched.add_job(func=TestScheduler.__my_job, trigger=self.__trigger,args=[self, 'text'])
 
     def start(self):
         self.__sched.start()
+
+    def stop(self):
+        self.__sched.shutdown()
+
 
 if __name__ == '__main__':
     t = TestScheduler()
